9a00b83410e0ea16849dcf3d51cf9121fb622ee2
shulaxshan <shulazshan@gmail.com>
2024-06-19 23:33:32 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,15 +1,15 @@
 import sys
-from logger import logging
+from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+    file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
-    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -20,11 +20,11 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__=="__main__":
+# if __name__=="__main__":
 
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide  by Zero")
-        raise CustomException(e,sys)
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide  by Zero")   
+#         raise CustomException(e,sys)
     
