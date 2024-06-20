b00b085bb27c8e592331e1ebf904acbc9e48c089
shulaxshan <shulazshan@gmail.com>
2024-06-20 15:46:59 +0530

    data transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,15 @@ from src.logger import logging
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+############################
+##### set PYTHONPATH=%cd%
+##### python src/components/data_ingestion.py
+############# i had a issue when importing src.exception. Issues thrown as No module exception. The reason is it not read the out of src folder files. Therfore i used "export PYTHONPATH=$(pwd)"
+##### You can set the PYTHONPATH environment variable to include the src directory before running your script. This will allow Python to find the src module.
+#############################
 
 @dataclass
 class DataIngestionConfig:
@@ -33,7 +42,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -45,4 +54,9 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+
+
